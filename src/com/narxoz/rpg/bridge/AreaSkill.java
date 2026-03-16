e7c6055db51d33d34ca92a02784e3f5d582620b3
Ayaulym <ayaulym.serikova12@narxoz.kz>
2026-03-16 10:11:27 +0500

    Implement Bridge behavior for single target and area skills

--- src/com/narxoz/rpg/bridge/AreaSkill.java
+++ src/com/narxoz/rpg/bridge/AreaSkill.java
@@ -9,8 +9,8 @@ public class AreaSkill extends Skill {
 
     @Override
     public void cast(CombatNode target) {
-        // TODO: Area Bridge action
-        // Apply resolved damage to a composite target.
-        // Tip: Let Composite classes decide how to distribute AOE damage.
+        if (target!=null && target.isAlive()) {
+            target.takeDamage(resolvedDamage());
+        }
     }
 }
